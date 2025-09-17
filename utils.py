1195fcfd5ca268509fe1ab9528252404d2f4bf85
guodong zhu <79653701+ZHUGUODONG1@users.noreply.github.com>
2025-09-17 16:07:14 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Aug 20 13:20:23 2018
 
-@author: gk
-"""
 import math
 import numpy as np
 import torch
@@ -13,27 +9,13 @@ from torch.autograd import Variable
 import sys
 from torch.nn import BatchNorm2d, Conv1d, Conv2d, Conv3d, ModuleList, Parameter, LayerNorm, BatchNorm1d, BatchNorm3d
 
-"""
-x-> [batch_num,in_channels,num_nodes,tem_size],
-"""
-
-
-##gwnet
-# class nconv(nn.Module):
-#     def __init__(self):
-#         super(nconv,self).__init__()
-
-#     def forward(self,x, A):
-#         A=A.transpose(-1,-2)
-#         x = torch.einsum('ncvl,vw->ncwl',(x,A))
-
 
 class DelayEstimators(nn.Module):
     def __init__(self, tem_size=12,c_in=32, in_dim=1, S_in_dim=4, dilation_channels=32):
         super(DelayEstimators, self).__init__()
         self.out_channels=dilation_channels
 
-        # Initialize convolution layers
+      
         self.conv_trend_Q = nn.Conv2d(c_in, dilation_channels, kernel_size=(1, 3), padding=(0, 1), stride=(1, 1),
                                     bias=True)
         self.conv_layers_K = nn.ModuleList(
@@ -53,30 +35,30 @@ class DelayEstimators(nn.Module):
         Returns:
             Tensor: The final aggregated output after applying convolutions, attention, and averaging.
         """
-        # Apply convolution to the trend input (current traffic)
+      
         B, C, N, T = CD.shape
         Q = self.conv_trend_Q(Trend).reshape(B, self.out_channels, -1).permute(0, 2, 1)
-        # Split C along the 2nd dimension (dim=1) into 4 parts
+       
         CD_split = torch.split(CD, 1, dim=1)
         agg = None
-        # Apply convolution and attention mechanism on each split of C
+        
         for i in range(C):
-            K = self.conv_layers_K[i](CD_split[i]).reshape(B, self.out_channels, -1)  # Apply convolution to the ith split
+            K = self.conv_layers_K[i](CD_split[i]).reshape(B, self.out_channels, -1)  
             V = self.conv_layers_V[i](CD_split[i]).reshape(B, self.out_channels, -1).permute(0, 2, 1)
             atten_wights =  F.softmax(torch.bmm(Q, K) * self.scale,dim=-1)
-            # Compute attention weights (softmax)
+           
             prod = torch.bmm(atten_wights, V).permute(0, 2, 1).reshape(B, self.out_channels, N, T)
-            # Accumulate the product in agg
+         
             if agg is None:
                 agg = prod
             else:
                 agg += prod
-        # Take the average of the accumulated product
+
         agg = agg / C
         return agg
 
 
-# 通道注意力机制模块 (CA)
+
 class ChannelAttention(nn.Module):
     def __init__(self, in_channels, reduction_ratio=2):
         super(ChannelAttention, self).__init__()
@@ -108,9 +90,9 @@ class Coss_Attention(nn.Module):
         attn = torch.bmm(Q, K) * self.scale
         attn = F.softmax(attn, dim=-1)
         out = torch.bmm(attn, V).permute(0, 2, 1).reshape(B, self.out_channels, N, T)
-        return out  # 残差连接
+        return out  
 
-# 自注意力机制 (SAT)
+
 class SelfAttention(nn.Module):
     def __init__(self, in_channels,out_channels):
         super(SelfAttention, self).__init__()
@@ -121,17 +103,16 @@ class SelfAttention(nn.Module):
         self.out_channels=out_channels
 
     def forward(self, x):
-        B, C, N, T = x.shape  # Batch size, Channels, Nodes, Time steps
+        B, C, N, T = x.shape 
         Q = self.query(x).reshape(B, self.out_channels, -1).permute(0, 2, 1)
         K = self.key(x).reshape(B, self.out_channels, -1)
         V = self.value(x).reshape(B, self.out_channels, -1).permute(0, 2, 1)
         attn = torch.bmm(Q, K) * self.scale
         attn = F.softmax(attn, dim=-1)
         out = torch.bmm(attn, V).permute(0, 2, 1).reshape(B, self.out_channels, N, T)
-        return out  # 残差连接
+        return out  #
 
 
-# 图卷积层 (GCN)
 class GCN(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(GCN, self).__init__()
@@ -154,11 +135,11 @@ class GCN1(nn.Module):
         out = torch.matmul(out, self.weight)
         return out
 
-# SAG Pooling 模块
+
 class SAGPool(nn.Module):
     def __init__(self, in_channels, length, pool_ratio=0.5):
         super(SAGPool, self).__init__()
-        self.gcn = GCN1(in_channels*length,1)  # 用于计算节点的重要性分数
+        self.gcn = GCN1(in_channels*length,1)  
         self.sigmoid = nn.Sigmoid()
         self.pool_ratio = pool_ratio
 
@@ -177,7 +158,7 @@ class SAGPool(nn.Module):
         return x_new
 
 
-# 多尺度图卷积网络 (使用 SAG Pooling)
+
 class MultiScaleGCN(nn.Module):
     def __init__(self, in_channels, out_channels, length, num_nodes, pool_ratio1=0.5,pool_ratio2=0.1,eb_dim=10):
         super(MultiScaleGCN, self).__init__()
@@ -187,15 +168,15 @@ class MultiScaleGCN(nn.Module):
         self.channel_attention1 = ChannelAttention(out_channels)
         self.channel_attention2 = ChannelAttention(out_channels)
         self.channel_attention3 = ChannelAttention(out_channels)
-        self.pool1 = SAGPool(in_channels,length, pool_ratio=pool_ratio1)  # SAG Pooling
-        self.pool2 = SAGPool(in_channels,length, pool_ratio=pool_ratio2)  # SAG Pooling
+        self.pool1 = SAGPool(in_channels,length, pool_ratio=pool_ratio1)  
+        self.pool2 = SAGPool(in_channels,length, pool_ratio=pool_ratio2)  
         self.self_attention = SelfAttention(3*out_channels,out_channels)
-        self.E1 = nn.Parameter(torch.randn(num_nodes, eb_dim))  # 动态矩阵 E1
-        self.E2 = nn.Parameter(torch.randn(eb_dim, num_nodes))  # 动态矩阵 E2
-        self.E11 = nn.Parameter(torch.randn(int(num_nodes*pool_ratio1), eb_dim))  # 动态矩阵 E1
-        self.E21 = nn.Parameter(torch.randn(eb_dim, int(num_nodes*pool_ratio1)))  # 动态矩阵 E2
-        self.E12 = nn.Parameter(torch.randn(int(num_nodes*pool_ratio2), eb_dim))  # 动态矩阵 E1
-        self.E22 = nn.Parameter(torch.randn(eb_dim, int(num_nodes*pool_ratio2)))  # 动态矩阵 E2
+        self.E1 = nn.Parameter(torch.randn(num_nodes, eb_dim))  
+        self.E2 = nn.Parameter(torch.randn(eb_dim, num_nodes))  
+        self.E11 = nn.Parameter(torch.randn(int(num_nodes*pool_ratio1), eb_dim))  
+        self.E21 = nn.Parameter(torch.randn(eb_dim, int(num_nodes*pool_ratio1))) 
+        self.E12 = nn.Parameter(torch.randn(int(num_nodes*pool_ratio2), eb_dim))  
+        self.E22 = nn.Parameter(torch.randn(eb_dim, int(num_nodes*pool_ratio2)))  
 
     def forward(self, x,adj):
         B, T, N, C = x.shape
@@ -215,14 +196,13 @@ class MultiScaleGCN(nn.Module):
         out3 = self.gcn3(out3, adj3)
         out3 = self.channel_attention3(out3)
 
-        # 上采样到原始大小
+
         out2_up = F.interpolate(out2, size=(out1.shape[2], out1.shape[3]), mode='bilinear', align_corners=True)
         out3_up = F.interpolate(out3, size=(out1.shape[2], out1.shape[3]), mode='bilinear', align_corners=True)
 
-        # 拼接多尺度结果
+
         multi_scale_out = torch.cat([out1, out2_up, out3_up], dim=1)
 
-        # 自注意力机制
         final_out = self.self_attention(multi_scale_out)
 
         return final_out
@@ -279,12 +259,13 @@ class cheby_conv(nn.Module):
             L0, L1 = L1, L2
             Ls.append(L2)
 
-        Lap = torch.stack(Ls, 0)  # [K,nNode, nNode]
+        Lap = torch.stack(Ls, 0)  
         # print(Lap)
         Lap = Lap.transpose(-1, -2)
         x = torch.einsum('bcnl,knq->bckql', x, Lap).contiguous()
         x = x.view(nSample, -1, nNode, length)
         out = self.conv1(x)
         return out
+
 
 
